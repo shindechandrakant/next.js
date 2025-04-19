d682e208234944a6595df57457a3be9bb125deff
Chandrakant <chandrakantshinde7348@gmail.com>
2025-04-19 12:42:11 +0530

    lesson: metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "Learning next",
+  description: "Theory lessons",
+};
+
 export default function About() {
   return <div>About</div>;
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,18 +1,32 @@
-"use client";
 import React from "react";
-import { useSearchParams } from "next/navigation";
+
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title: string = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Product is ${params.productId}`);
+    }, 1000);
+  });
+
+  console.log({ title });
+  return {
+    title: title,
+    description: `Detail page for ${params.productId}`,
+  };
+};
 
 export default function productDetail({
   params,
 }: {
   params: { productId: string };
 }) {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("name");
-
-  return (
-    <div>
-      ProductDetail {params.productId} - {id}
-    </div>
-  );
+  return <div>ProductDetail {params.productId}</div>;
 }
